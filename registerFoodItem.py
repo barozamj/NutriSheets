90ac1f3a945e7251df09a706537532288334d615
barozamj <barozamj@oregonstate.edu>
2023-11-06 23:43:53 -0800

    Update registerFoodItem.py
    updated to work with JSON files instead of csv

--- registerFoodItem.py
+++ registerFoodItem.py
@@ -1,18 +1,38 @@
-# registerFoodItem.py
+#registerFoodItem.py
+
+import json
 
 def register_food_item():
+    # Prompt for food item details
     name = input("Enter Food Item name: ")
     calories = input("Enter calories per serving: ")
     grams_per_serving = input("Enter grams/serving: ")
     net_weight = input("Enter food item net weight: ")
     cost = input("Enter food item cost: ")
 
-    # Construct the food item data string
-    food_item_data = f"{name},{calories},{grams_per_serving},{net_weight},{cost}\n"
+    # Create a dictionary for the food item
+    food_item = {
+        "name": name,
+        "calories": int(calories),  # Assuming calories are an integer
+        "grams_per_serving": float(grams_per_serving),  # Assuming this is a floating point number
+        "net_weight": float(net_weight),  # Assuming this is a floating point number
+        "cost": float(cost)  # Assuming cost is a floating point number
+    }
 
-    # Write the food item data to a local file
-    with open("food_items.csv", "a") as file:
-        file.write(food_item_data)
+    # Read the existing data and update it
+    try:
+        with open("food_items.json", "r") as file:
+            food_items = json.load(file)
+    except FileNotFoundError:
+        # If the file doesn't exist, start with an empty list
+        food_items = []
+
+    # Append the new food item
+    food_items.append(food_item)
+
+    # Write the updated data back to the file
+    with open("food_items.json", "w") as file:
+        json.dump(food_items, file, indent=4)
 
     print("Food item registered successfully!")
 
